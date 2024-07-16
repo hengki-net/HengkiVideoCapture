c6177c7e6bcf56044448ea8071260cbe99147643
hengki <hyeongi.hong@vntgcorp.com>
2024-07-16 15:36:07 +0900

    upload

--- Lecture04/Program.cs
+++ Lecture04/Program.cs
@@ -7,7 +7,8 @@ namespace Lecture04
 {
     internal class Program
     {
-        public static bool _saveSwitch = false;
+        private static bool _saveSwitch = false;    // 이미지 저장용 스위치. true로 바뀌면 저장되고 false로 되돌아온다.
+        private static long _lastCaptureTime = 0;   // 마지막 이미지 저장 시간을 저장. 저장 딜레이를 주기 위한 변수이다.
 
         static void Main()
         {
@@ -16,12 +17,16 @@ namespace Lecture04
         }
 
         /// <summary>
+        /// 
+        /// 
+        /// 
         /// 비디오 캡처 처리
         /// </summary>
         private static void ProcessVideoCapture()
         {
             // RTSP 영상 가져오기
             VideoCapture capture = new VideoCapture();
+            //capture.Open("rtsp://admin:@park0101@172.31.18.162:558/LiveChannel/1/media.smp");
             capture.Open("rtsp://admin:WBF460-8660@172.31.18.158:558/LiveChannel/2/media.smp");
 
             Mat originalMat = new Mat();
@@ -42,12 +47,12 @@ namespace Lecture04
                     HandleingCaptureImage(ref originalMat, out handlingMat);
 
                     // 픽셀 카운트
-                    _saveSwitch = CountPixelOnBoundaryUpper(ref handlingMat, boundary: 200, red: 100);
+                    _saveSwitch = observeImageCapture(ref handlingMat, boundary: 200, red: 100);
 
                     // 이미지 저장
                     if (_saveSwitch)
                     {
-                        SaveCaptureImageToFile(ref originalMat, @$"c:\{DateTime.Now.ToString("yyyyMMddHHmmssfff")}.jpg");
+                        //SaveCaptureImageToFile(ref originalMat, @$"c:\{DateTime.Now.ToString("yyyyMMddHHmmssfff")}.jpg");
                         _saveSwitch = false;
                     }
 
@@ -57,7 +62,7 @@ namespace Lecture04
 
                     // 영상 보이기
                     Cv2.ImShow("Show1", originalMat);
-                    Cv2.ImShow("Show2", handlingMat);
+                    //Cv2.ImShow("Show2", handlingMat);
 
                     if (Cv2.WaitKey(1) >= 0) break;
                 }
@@ -97,7 +102,8 @@ namespace Lecture04
         private static void printContentOnImage(ref Mat originalMat)
         {
             // 글자쓰기
-            Cv2.PutText(originalMat, $"Hello", new OpenCvSharp.Point(10, 30), HersheyFonts.HersheySimplex, 1, Scalar.LightGreen, 1, LineTypes.AntiAlias);
+            Cv2.PutText(originalMat, $"Hello", new OpenCvSharp.Point(10, 30), 
+                            HersheyFonts.HersheySimplex, 1, Scalar.LightGreen, 1, LineTypes.AntiAlias);
 
             // 사각형 그리기
             Cv2.Rectangle(originalMat, new Rect(1780, 400, 50, 100), Scalar.GreenYellow, 1);
@@ -124,18 +130,27 @@ namespace Lecture04
         }
 
         /// <summary>
-        /// 픽셀 카운트 
+        /// 이미지 저장 신호 생성
         /// </summary>
         /// <param name="mat">이미지 프레임</param>
-        /// <param name="boundary">경계를 넘는 픽셀 갯수에 따라 센싱 유무를 표시한다.</param>
+        /// <param name="boundary">경계를 넘는 픽셀 갯수에 따라 캡쳐 유무를 결정한다.</param>
         /// <param name="red">빨간색 경계</param>
         /// <param name="green">녹색 경계</param>
         /// <param name="blue">파란색 경계</param>
-        /// <returns>센싱결과를 true/false로 출력한다.</returns>
-        private static bool CountPixelOnBoundaryUpper(ref Mat mat, int boundary = 50, int red = 0, int green = 0, int blue = 0)
+        /// <returns>켭쳐 할지를 true/false로 반환한다.</returns>
+        private static bool observeImageCapture(ref Mat mat, int boundary = 50, int red = 0, int green = 0, int blue = 0)
         {
-            Bitmap bitmap = mat.ToBitmap();
+            long nowTime = UnixTimeNow();
 
+            if (nowTime - _lastCaptureTime < 10)
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("다다닥 저장되는 캡쳐 딜레이..");
+                return false;
+            }            
+
+
+            Bitmap bitmap = mat.ToBitmap();
             int pixelCount = 0;
 
             for (int i = 0; i < bitmap.Width; i++)
@@ -153,14 +168,28 @@ namespace Lecture04
                 }
             }
 
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"센싱값 : {pixelCount}");
+
             if (boundary < pixelCount)
             {
+                _lastCaptureTime = nowTime;
                 return true;
             }
             else
             {
                 return false;
             }
+        }
+
+        /// <summary>
+        /// 유닉스 타임스탬프 (1970년 1월1일 이후의 초)
+        /// </summary>
+        /// <returns></returns>
+        public static long UnixTimeNow()
+        {
+            var timeSpan = (DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0));
+            return (long)timeSpan.TotalSeconds;
         }
 
     }
---
+++ Lecture05/Program.cs
@@ -0,0 +1,226 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System.Data;
+using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace Lecture05
+{
+    internal class Program
+    {        
+        private const string ORIGINAL_FOLDER_PATH = @"C:\test";
+        private const string NAS_FOLDER_PATH = @"C:\test1";
+        private const string ERROR_FOLDER_PATH = @"C:\test2";
+        private const string PYTHON_PATH = @"C:\Python312\python.exe";
+        private const string MODEL_PATH = @"C:\pytest\helloworld.py";
+        private const string DB_CONNECTION_STRING = @"USER ID=L2TEST;PASSWORD=L2TEST;DATA SOURCE=211.230.146.99/L2SERVICES;";
+
+        private static OracleConnection _conn = new OracleConnection(DB_CONNECTION_STRING);
+
+        static void Main(string[] args)
+        {
+            // 초기화 
+            if (!InitProgram())
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("강제 종료...");
+                Environment.Exit(0);
+            }
+
+            // 반복 실행
+            while (true)
+            {
+                ProcessFolderMoveAndDbInsert();
+                Thread.Sleep(3000);
+            }
+        }
+
+        /// <summary>
+        /// 프로그램 초기화
+        /// </summary>
+        /// <returns>초기화 성공시 true, 실패시 false 반환</returns>
+        private static bool InitProgram()
+        {
+            try
+            {
+                FileInfo fileInfo = new FileInfo(ORIGINAL_FOLDER_PATH);                
+                if (!fileInfo.Exists)
+                {
+                    DirectoryInfo directory = new DirectoryInfo(ORIGINAL_FOLDER_PATH);
+                    directory.Create();
+                }
+
+                FileInfo fileInfo2 = new FileInfo(NAS_FOLDER_PATH);
+                if (!fileInfo2.Exists)
+                {
+                    DirectoryInfo directory = new DirectoryInfo(NAS_FOLDER_PATH);
+                    directory.Create();
+                }
+
+                FileInfo fileInfo3 = new FileInfo(ERROR_FOLDER_PATH);
+                if (!fileInfo3.Exists)
+                {
+                    DirectoryInfo directory = new DirectoryInfo(ERROR_FOLDER_PATH);
+                    directory.Create();
+                }
+
+                
+
+                if (!CheckDbConnection())
+                {
+                    return false;
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// DB 연결 확인
+        /// </summary>
+        /// <returns>연결상태 true/false 반환</returns>
+        private static bool CheckDbConnection()
+        {
+            try
+            {
+                if (_conn == null)
+                {
+                    _conn = new OracleConnection(DB_CONNECTION_STRING);
+                }
+
+                if (_conn.State == ConnectionState.Closed)
+                {
+                    _conn.Open();
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                {
+                    Console.WriteLine(ex.ToString());
+                    return false;
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// 이미지파일 이동 및 데이터 베이스 실적 생성
+        /// </summary>
+        private static void ProcessFolderMoveAndDbInsert()
+        {
+            string[] originalFiles = Directory.GetFiles(ORIGINAL_FOLDER_PATH);
+
+            foreach (string originalFile in originalFiles)
+            {
+                // 하루는 오전 6시에 시작한다.
+                DateTime nTime = DateTime.Now.AddHours(-6);
+                string y = nTime.ToString("yyyy");
+                string m = nTime.ToString("MM");
+                string d = nTime.ToString("dd");
+
+                // 01. 경로 정의
+                FileInfo originalFileInfo = new FileInfo(originalFile);
+                FileInfo moveFileInfo = new FileInfo($@"{NAS_FOLDER_PATH}\{y}\{m}\{d}\{originalFileInfo.Name}");
+
+                // 02. 이동할 폴더 체크                              
+                if (!moveFileInfo.Directory.Exists)
+                {
+                    moveFileInfo.Directory.Create();
+                }
+
+                // 03. 파이선 호출
+                string pythonResult = CallPython(originalFileInfo.FullName);
+
+                // 04. DB 저장
+                if (!DbInsertMes(moveFileInfo, pythonResult))
+                {
+                    // 05. 저장 실패시 에러폴더로 이동
+                    File.Move(originalFileInfo.FullName, ERROR_FOLDER_PATH, true);
+                    continue;
+                }                
+
+                // 06. NAS 폴더로 파일 이동
+                File.Move(originalFileInfo.FullName, moveFileInfo.FullName, true);
+            }
+        }
+
+        /// <summary>
+        /// DB TEST000 데이터 저장
+        /// </summary>
+        /// <param name="fileInfo">저장할 파일 정보</param>
+        /// <returns>데이터 정상 저장시 true, 실패시 false</returns>
+        private static bool DbInsertMes(FileInfo fileInfo, string pythonResult)
+        {
+            try
+            {
+                string serverPath = fileInfo.FullName.Replace(NAS_FOLDER_PATH, "").Replace(@"\", @"/");
+                string url = $"http://localhost{serverPath}";
+
+                // 저장
+                string query = $@" INSERT INTO TEST000 (COL1, COL2, COL3, COL4 ) VALUES ( :P_COL1, :P_COL2, :P_COL3, :P_COL4) ";
+
+                OracleCommand cmd = new OracleCommand();
+                cmd.Connection = _conn;
+                cmd.CommandText = query;
+                cmd.Parameters.Clear();
+                cmd.Parameters.Add("P_COL1", fileInfo.Name);
+                cmd.Parameters.Add("P_COL2", url);
+                cmd.Parameters.Add("P_COL3", DateTime.Now);
+                cmd.Parameters.Add("P_COL4", pythonResult);
+                int rst = cmd.ExecuteNonQuery();
+                cmd.Dispose();
+
+                if (rst == 0)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 파이썬 호출
+        /// </summary>
+        /// <param name="arg1"></param>
+        /// <returns></returns>
+        static private string CallPython(string arg)
+        {
+            var psi = new ProcessStartInfo
+            {
+                // 파이썬 설치 경로
+                FileName = PYTHON_PATH,
+                Arguments = $@"{MODEL_PATH} {arg}",
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
+
+            var erros = string.Empty;
+            var results = string.Empty;
+
+            using (Process process = Process.Start(psi))
+            {
+                using (StreamReader reader = process.StandardOutput)
+                {
+                    erros = process.StandardError.ReadToEnd();
+                    results = process.StandardOutput.ReadToEnd();
+                }
+            }
+
+            return results;
+        }
+    }
+}
